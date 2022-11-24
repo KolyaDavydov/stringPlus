1beb6bf700d05f78c9a97ea38950a707e58e9898
Kolya Davydov <kolya.davydov@inbox.ru>
2022-11-24 14:34:07 +0300

    implemented s21_memset

--- src/s21_string.c
+++ src/s21_string.c
@@ -49,3 +49,17 @@ void *s21_memmove(void *dest, const void *src, s21_size_t n) {
 
   return result;
 }
+
+/**
+    5. Copies the character c (an unsigned char) to the first n characters of
+    the string pointed to, by the argument str.
+
+    Implemented by: Almeta Terry
+**/
+void *s21_memset(void *str, int c, s21_size_t n) {
+  char *ptr = str;
+  for(s21_size_t i = 0; i < n; i++) {
+    *(ptr + i) = c;
+  }
+  return ptr;
+}
